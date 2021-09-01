9aaf5b8b9186ad9ea4d6f1859b76d6055740f150
Lucas Ferreira Machado <lucasferreiramachadopro@gmail.com>
2021-09-01 12:21:35 -0300

    Improve Code Coverage: Refactor ImageUrlProtocol. Remove unused methods.

--- Interview/Application/SceneDelegate.swift
+++ Interview/Application/SceneDelegate.swift
@@ -1,20 +1,26 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
     var window: UIWindow?
-
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
-
-            let viewController = ListContactsViewAssembler.assemble()
-            window.rootViewController = UINavigationController(rootViewController: viewController)
             self.window = window
-            window.makeKeyAndVisible()
-            
+            let module = assembleFirstModule()
+            startModule(module: module)
         }
     }
 }
 
+// MARK: Modules
+extension SceneDelegate {
+
+    func assembleFirstModule() -> UIViewController {
+        ListContactsViewAssembler.assemble()
+    }
+
+    func startModule(module: UIViewController) {
+        self.window?.rootViewController = UINavigationController(rootViewController: module)
+        self.window?.makeKeyAndVisible()
+    }
+}
