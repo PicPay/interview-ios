c0a0c87890ee51188fa18fb81266f6bb35bfb475
Bruno Cavaletti Alves <bcalves@apoioprodesp.gov.sp.br>
2023-10-17 14:49:49 -0300

    refact: decodable model

--- Interview/Scenes/ListContacts/Models/Contact.swift
+++ Interview/Scenes/ListContacts/Models/Contact.swift
@@ -11,20 +11,8 @@ import Foundation
 ]
 */
 
-class Contact: Codable {
-    var id: Int
-    var name: String = ""
-    var photoURL = ""
-    
-    init(id: Int, name: String, photoURL: String) {
-        self.id = id
-        self.name = name
-        self.photoURL = photoURL
-    }
-    
-    enum CodingKeys: String, CodingKey {
-        case name = "name"
-        case photoURL = "photoURL"
-        case id = "id"
-    }
+struct Contact: Decodable {
+    let id: Int
+    let name: String
+    let photoURL: String
 }
---
+++ Interview/Scenes/ListContacts/ViewModels/ListContactViewData.swift
@@ -0,0 +1,9 @@
+//
+//  ListContactViewData.swift
+//  Interview
+//
+//  Created by Bruno Alves on 17/10/23.
+//  Copyright © 2023 PicPay. All rights reserved.
+//
+
+import Foundation
